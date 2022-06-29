8d3308f3a47abe9ed1395240fcd92a8b461d0d14
BlupBlob <106178659+blupblob@users.noreply.github.com>
2022-06-29 15:39:00 +0200

    ctrl+c toegevoegd aan wolfsense

--- winutil.ps1
+++ winutil.ps1
@@ -38,7 +38,7 @@ else{
 }
 Write-Host "Script is nog steeds in de Alfa fase. Conctact kan worden opgenomen via ict@wolfsense.nl"
 
-Read-Host -Prompt "Druk op ENTER om door te gaan" 
+Read-Host -Prompt "Druk op ENTER om door te gaan of CTRL+C om te stoppen" 
 
 
 # $inputXML = Get-Content "MainWindow.xaml" #uncomment for development
