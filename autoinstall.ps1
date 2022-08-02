49ee58025aee6c04771603c756a69088b3f9e4b8
BlupBlob <106178659+blupblob@users.noreply.github.com>
2022-08-02 10:04:17 +0200

    test1

--- autoinstall.ps1
+++ autoinstall.ps1
@@ -57,7 +57,8 @@ Foreach ($app in $apps) {
     $listApp = winget list --exact -q $app.name
     if (![String]::Join("", $listApp).Contains($app.name)) {
         Write-host "Installeren: " $app.name
-        winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+	 Start-Process powershell.exe -Verb RunAs -ArgumentList "-command winget install -e --accept-source-agreements --accept-package-agreements --silent $app.name | Out-Host" -Wait -WindowStyle Maximized
+    #    winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
     }
     else {
         Write-host "Skipping: " $app.name " (already installed)"
