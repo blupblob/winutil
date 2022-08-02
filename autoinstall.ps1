2a7beb64241976cad6ab11a283c4f4b5508d65d8
BlupBlob <106178659+blupblob@users.noreply.github.com>
2022-08-02 11:09:20 +0200

    test4

--- autoinstall.ps1
+++ autoinstall.ps1
@@ -1,66 +1,131 @@
-<#
-.NOTES
-   Author      : WolfSense IT Security
-   GitHub      : https://github.com/blupblop
-    Version 0.0.1
-#>
+# Created by Kugane
 
-$ErrorActionPreference = 'SilentlyContinue'
-$wshell = New-Object -ComObject Wscript.Shell
-$Button = [System.Windows.MessageBoxButton]::YesNoCancel
-$ErrorIco = [System.Windows.MessageBoxImage]::Error
-If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
-	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
-	Exit
-}
 
-# WinGet controle
-Write-Host "Kijken of Winget geinstalleerd is..."
-
-# Er wordt gecontroleerd of Winget is geinstalleerd
-if (Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe){
-    'Winget is geinstalleerd'
-}  
-else{
-    # Winget wordt geinstalleerd vanuit de Microsoft Store
-	Write-Host "Winget is niet gevonden, het installeren begint nu."
-	Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget" /quiet
-	$nid = (Get-Process AppInstaller).Id
-	Wait-Process -Id $nid
-	Write-Host Winget is geinstalleerd
-    $ResultText.text = "`r`n" +"`r`n" + "Winget is nu geinstalleerd"
-}
-
-Write-Output "Apps installeren"
-$apps = @(
-    @{name = "7zip.7zip" },
-    @{name = "Adobe.Acrobat.Reader.64-bit" },
-    @{name = "Google.Chrome" },
-#   @{name = "Famatech.AdvancedIPScanner" },
-    @{name = "Notepad++.Notepad++" },
-    @{name = "PuTTY.PuTTY" },
-    @{name = "AnyDeskSoftwareGmbH.AnyDesk" },
-    @{name = "TeamViewer.TeamViewer" },
-   @{name = "Fortinet.FortiClientVPN" },
-   @{name = "TeamViewer.TeamViewer" },
-#   @{name = "Greenshot.Greenshot" },
-#   @{name = "Microsoft.dotnet" },
-#   @{name = "Microsoft.PowerShell" },
-#   @{name = "Microsoft.PowerToys" },
-#   @{name = "Microsoft.WindowsTerminal" },
-#   @{name = "Mozilla.Firefox" },
-    @{name = "Microsoft.Office" },
-    @{name = "Cisco.WebexTeams" }
-#   @{name = "Zoom.Zoom" }
+### Here can you add apps that you want to configure during installation ###
+# just add the app id from winget
+$graphical = @(
+    @{name = "ClamWin.ClamWin" }
 );
-Foreach ($app in $apps) {
-    $listApp = winget list --exact -q $app.name
-    if (![String]::Join("", $listApp).Contains($app.name)) {
-        Write-host "Installeren: " $app.name
-	 Start-Process powershell.exe -Verb RunAs -ArgumentList "-command winget install -e --accept-source-agreements --accept-package-agreements --silent $app.name | Out-Host" -Wait 
-    #    winget install -e -h --accept-source-agreements --accept-package-agreements --id $app.name 
+
+### These apps are installed silently for all users ###
+# for msstore apps you need to specify the source like below
+
+$apps = @(
+    @{name = "7zip.7zip" }
+    @{name = "Google.Chrome" }
+    @{name = "Notepad++.Notepad++" },
+    @{name = "PuTTY.PuTTY" }
+    @{name = "AnyDeskSoftwareGmbH.AnyDesk" }
+    @{name = "TeamViewer.TeamViewer" }
+    @{name = "Fortinet.FortiClientVPN" }
+    @{name = "TeamViewer.TeamViewer" }
+    @{name = "Mozilla.Firefox" }
+    @{name = "Microsoft.Office" }
+    @{name = "Cisco.WebexTeams" }
+    @{name = "9NCBCSZSJRSB"; source = "msstore" }        # Spotify
+    @{name = "9NKSQGP7F2NH"; source = "msstore" }        # Whatsapp Desktop
+    @{name = "9WZDNCRFJ3TJ"; source = "msstore" }        # Netflix
+    @{name = "9P6RC76MSMMJ"; source = "msstore" }        # Prime Video
+);
+
+#############################################################################################
+################################ Don't change anything below ################################
+#############################################################################################
+
+### Install WinGet ###
+# Based on this gist: https://gist.github.com/crutkas/6c2096eae387e544bd05cde246f23901
+$hasPackageManager = Get-AppxPackage -Name 'Microsoft.Winget.Source' | Select Name, Version
+$hasVCLibs = Get-AppxPackage -Name 'Microsoft.VCLibs.140.00.UWPDesktop' | Select Name, Version
+$hasXAML = Get-AppxPackage -Name 'Microsoft.UI.Xaml.2.7*' | Select Name, Version
+$hasAppInstaller = Get-AppxPackage -Name 'Microsoft.DesktopAppInstaller' | Select Name, Version
+$DesktopPath = [System.Environment]::GetFolderPath([System.Environment+SpecialFolder]::Desktop)
+$errorlog = winget_error.log
+
+    Write-Host -ForegroundColor Yellow "Checking if WinGet is installed"
+    if (!$hasPackageManager) {
+            if ($hasVCLibs.Version -lt "14.0.30035.0") {
+                Write-Host -ForegroundColor Yellow "Installing VCLibs dependencies..."
+                Add-AppxPackage -Path "https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx"
+                Write-Host -ForegroundColor Green "VCLibs dependencies successfully installed."
+            }
+            else {
+                Write-Host -ForegroundColor Green "VCLibs is already installed. Skip..."
+            }
+            if ($hasXAML.Version -lt "7.2203.17001.0") {
+                Write-Host -ForegroundColor Yellow "Installing XAML dependencies..."
+                Add-AppxPackage -Path "https://github.com/Kugane/winget/raw/main/Microsoft.UI.Xaml.2.7_7.2203.17001.0_x64__8wekyb3d8bbwe.Appx"
+                Write-Host -ForegroundColor Green "XAML dependencies successfully installed."
+            }
+            else {
+                Write-Host -ForegroundColor Green "XAML is already installed. Skip..."
+            }
+            if ($hasAppInstaller.Version -lt "1.16.12653.0") {
+                Write-Host -ForegroundColor Yellow "Installing WinGet..."
+    	        $releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+    		    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    		    $releases = Invoke-RestMethod -Uri "$($releases_url)"
+    		    $latestRelease = $releases.assets | Where-Object { $_.browser_download_url.EndsWith("msixbundle") } | Select-Object -First 1
+    		    Add-AppxPackage -Path $latestRelease.browser_download_url
+                Write-Host -ForegroundColor Green "WinGet successfully installed."
+            }
     }
     else {
-        Write-host "Skipping: " $app.name " (already installed)"
-    }
+        Write-Host -ForegroundColor Green "WinGet is already installed. Skip..."
+        }
+    Pause
+    Clear-Host
 }
+
+### Install Apps silent ###
+    Write-Host -ForegroundColor Cyan "Installing new Apps"
+    Foreach ($app in $apps) {
+        $listApp = winget list --exact -q $app.name
+        if (![String]::Join("", $listApp).Contains($app.name)) {
+            Write-Host -ForegroundColor Yellow  "Install:" $app.name
+            # MS Store apps
+            if ($app.source -ne $null) {
+                winget install --exact --silent --accept-source-agreements --accept-package-agreements $app.name --source $app.source
+                if ($LASTEXITCODE -eq 0) {
+                    Write-Host -ForegroundColor Green $app.name "successfully installed."
+                }
+                else {
+                    $app.name + " couldn't be installed." | Add-Content "$DesktopPath\$errorlog"
+                    Write-Host
+                    Write-Host -ForegroundColor Red $app.name "couldn't be installed."
+                    Write-Host -ForegroundColor Yellow "Write in $DesktopPath\$errorlog"
+                    Write-Host
+                    Pause
+                }    
+            }
+            # All other Apps
+            else {
+                winget install --exact --silent --scope machine --accept-source-agreements --accept-package-agreements $app.name
+                if ($LASTEXITCODE -eq 0) {
+                    Write-Host -ForegroundColor Green $app.name "successfully installed."
+                }
+                else {
+                    $app.name + " couldn't be installed." | Add-Content "$DesktopPath\$errorlog"
+                    Write-Host
+                    Write-Host -ForegroundColor Red $app.name "couldn't be installed."
+                    Write-Host -ForegroundColor Yellow "Write in $DesktopPath\$errorlog"
+                    Write-Host
+                    Pause
+                }  
+            }
+        }
+        else {
+            Write-Host -ForegroundColor Yellow "Skip installation of" $app.name
+        }
+    }
+    Pause
+    Clear-Host
+}
+
+### Finished ###
+function finish {
+    Write-Host
+    Write-Host -ForegroundColor Magenta  "Installation finished"
+    Write-Host
+    Pause
+}
+
