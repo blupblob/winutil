5d969af36dd0416d46a7dbf5973908e142bdaea2
BlupBlob <106178659+blupblob@users.noreply.github.com>
2022-08-02 11:16:47 +0200

    test5

--- autoinstall.ps1
+++ autoinstall.ps1
@@ -1,3 +1,15 @@
+# geen winget ? no problemo
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+
+$ErrorActionPreference = 'SilentlyContinue'
+$wshell = New-Object -ComObject Wscript.Shell
+$Button = [System.Windows.MessageBoxButton]::YesNoCancel
+$ErrorIco = [System.Windows.MessageBoxImage]::Error
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
 
 $apps = @(
     @{name = "7zip.7zip" }
